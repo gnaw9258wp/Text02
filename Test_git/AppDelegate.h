1dcad2330f9aa2006d4e910b5cd755826b936394
王鹏2157号 <wangpeng2157@ct108.com>
2018-03-03 21:27:11 +0800

    初始化项目

---
+++ Test_git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_git
+//
+//  Created by 王鹏 on 2018/3/3.
+//  Copyright © 2018年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_git
+//
+//  Created by 王鹏 on 2018/3/3.
+//  Copyright © 2018年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
